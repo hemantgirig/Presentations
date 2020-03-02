91c227831d194f5550af8a07df51c908f6d65c59
Hemantgiri S. Goswami <ghemant@gmail.com>
2020-03-02 16:06:31 +0530

    Add files via upload

---
+++ CommonMistakes/007_IndexDegradePerformance.sql
@@ -0,0 +1,30 @@
+USE AdventureWorks2008R2
+GO
+
+SET STATISTICS IO ON
+
+SELECT salesorderdetailID,orderqty
+FROM sales.SalesOrderDetail sod
+WHERE productid =
+	(
+		SELECT AVG(productid) FROM sales.SalesOrderDetail sod1
+		WHERE sod.SalesOrderID=sod1.SalesOrderID
+		GROUP BY SalesOrderID
+	)
+
+-- Index 1
+CREATE NONCLUSTERED INDEX [IX_TestIndex1]
+ON sales.salesorderdetail
+(salesorderid ASC, productid ASC)
+INCLUDE (salesorderdetailid,orderqty)
+
+
+-- Index 2
+CREATE NONCLUSTERED INDEX [IX_TestIndex2]
+ON sales.salesorderdetail
+(productid ASC, salesorderid ASC )
+INCLUDE (salesorderdetailid,orderqty)
+
+DROP INDEX  IX_TESTINDEX2 ON SALES.SALESORDERDETAIL
+GO
+DROP INDEX IX_TESTINDEX1 ON SALES.SALESORDERDETAIL
